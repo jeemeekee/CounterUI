faa4d4dfa3d19362667fa14c2395a8d9899f45a8
jeemeekee <jeemeekee@gmail.com>
2024-06-05 10:43:57 +0900

    Initial Commit

---
+++ CounterUI/CounterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterUIApp.swift
+//  CounterUI
+//
+//  Created by kimjimin on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CounterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
