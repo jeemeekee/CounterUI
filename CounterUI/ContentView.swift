b373c688a59d0bfaa87afb4b1c8367e45c7898fa
jeemeekee <jeemeekee@gmail.com>
2024-06-06 15:17:53 +0900

    add buttons

--- CounterUI/ContentView.swift
+++ CounterUI/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(count)")
+                .font(.largeTitle)
+                .padding(80)
+            
+            HStack {
+                Button("Plus one") {
+                    count += 1
+                }
+                
+                Spacer()
+                
+                Button("Plus Five") {
+                    count += 5
+                }
+                
+                Spacer()
+                
+                Button("Plus Ten") {
+                    count += 10
+                }
+            }
+            .padding(40)
+            
+            Button("Minus one") {
+                if count > 0 {
+                    count -= 1
+                }
+            }
+            .padding(40)
+            
+            Button("Reset") {
+                count = 0
+            }
         }
         .padding()
     }
